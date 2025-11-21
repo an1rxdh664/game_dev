77c55105b2c74f5762e18fd69aed3eb59e329076
an1rxdh664 <anirudhhh637@gmail.com>
2025-11-21 23:58:15 +0530

    partially working collisions

--- index.js
+++ index.js
@@ -65,15 +65,43 @@ playerImage.src = './images/playerDown.png';
 // Player movement through keys
 
 class Sprite{
-    constructor({position, velocity, image}){
+    constructor({position, velocity, image, frames = { max: 1 }}){
         this.position = position;
         this.image = image;
+        this.frames = frames;
+
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.max;
+            this.height = this.image.height;
+        }
     }
 
     draw() {
-        context.drawImage(this.image, this.position.x, this.position.y);
+        context.drawImage(this.image,
+            0,
+            0,
+            this.image.width / this.frames.max,
+            this.image.height,
+
+            this.position.x,
+            this.position.y,
+
+            this.image.width / this.frames.max,
+            this.image.height
+        );
     }
 }
+
+const player = new Sprite({
+    position: {
+        x: canvas.width / 2 - (192 / 3) / 1.75,
+        y: canvas.height / 2 - 68 / 2
+    },
+    image: playerImage,
+    frames: {
+        max: 4
+    }
+})
 
 const background = new Sprite({
     position: {
@@ -112,20 +140,15 @@ function animate(){
     // })
 
     testBoundary.draw();
-
-    context.drawImage(playerImage,
-        0,
-        0,
-        playerImage.width / 4,
-        playerImage.height,
-
-        canvas.width / 2 - (playerImage.width / 3) / 1.75, 
-        canvas.height / 2 - playerImage.height / 2,
-
-        playerImage.width / 4,
-        playerImage.height
-    );
+    player.draw();
+    
     // now to animate our playerImage recursively, i have to shift the image.onload() function into my animate function
+
+    if(player.position.x + player.width >= testBoundary.position.x 
+        && 
+        player.position.x <= testBoundary.position.x + testBoundary.width){
+        console.log('Colliding');
+    }
 
     if(keys.w.pressed && lastKey === 'w') {
         movables.forEach((movable) => {
